2674a0e33e37eb939919a16df8f0b1a832f640c8
rohpan <panugant@seattleu.edu>
2018-06-05 19:49:45 -0700

    lol

---
+++ my-applications.php
@@ -0,0 +1,36 @@
+<?php include './scripts/mysql_start.php'; ?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>My Application</title>
+</head>
+<body>
+<h1>My Application</h1>
+
+<?php
+$sql = "SELECT app_id, student_type_id, college_id, degree_id, major_id, term_id FROM application WHERE user_id = ".$user_id.";";
+$result = mysqli_query($conn,$sql);
+
+if(msqli_num_row($result) > 0){
+	echo "<table border = '1'>\n";
+
+	echo "<tr>\n";
+	echo "<th>App ID</th><th>Student Type</th><th>College</th><th>Degree</th><th>Major</th><th>Term</th>";
+	echo "</tr>\n";
+
+	while($row = mysqli_fetch_row($result)){
+		echo "<tr>\n";
+		echo "<td>".$row[0]."</td>\n"."<td>".$row[1]."</td>\n"."<td>".$row[2]."</td>\n"."<td>".$row[3]."</td>\n"."<td>".$row[4]."</td>\n"."<td>".$row[5]."</td>\n";
+		echo "</tr>\n";
+	}
+}
+
+else {
+	echo "No Applications"
+} 
+
+?>
+
+</body>
+</html>
+<?php include './scripts/mysql_close.php'; ?>
